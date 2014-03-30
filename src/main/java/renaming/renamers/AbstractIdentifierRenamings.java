942e745af81103aa2f6f3ca8ffd32835057d7745
Miltos Allamanis <m.allamanis@ed.ac.uk>
2014-03-30 14:02:33 +0100

    At last, identifier renamings can now handle the new token bound format.

--- src/main/java/renaming/renamers/AbstractIdentifierRenamings.java
+++ src/main/java/renaming/renamers/AbstractIdentifierRenamings.java
@@ -14,8 +14,8 @@ import java.util.logging.Logger;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
 
-import codemining.languagetools.ITokenizer.FullToken;
 import codemining.languagetools.Scope;
+import codemining.languagetools.TokenNameBinding;
 import codemining.lm.ngram.AbstractNGramLM;
 import codemining.lm.ngram.NGram;
 
@@ -58,8 +58,8 @@ public abstract class AbstractIdentifierRenamings implements
 	 */
 	@Override
 	public SortedSet<Renaming> calculateScores(
-			final Multiset<NGram<String>> ngrams, final Set<String> alternatives,
-			final Scope scope) {
+			final Multiset<NGram<String>> ngrams,
+			final Set<String> alternatives, final Scope scope) {
 		final SortedSet<Renaming> scoreMap = Sets.newTreeSet();
 
 		for (final String identifierName : alternatives) {
@@ -70,7 +70,7 @@ public abstract class AbstractIdentifierRenamings implements
 							ngram.getElement(), WILDCARD_TOKEN, identifierName);
 					final double ngramScore = scoreNgram(identNGram);
 					score += DoubleMath.log2(ngramScore) * ngram.getCount();
-				} catch (Throwable e) {
+				} catch (final Throwable e) {
 					LOGGER.warning(ExceptionUtils.getFullStackTrace(e));
 				}
 			}
@@ -82,8 +82,65 @@ public abstract class AbstractIdentifierRenamings implements
 		return scoreMap;
 	}
 
+	/**
+	 * @param relevantNgrams
+	 * @param currentName
+	 * @return
+	 */
+	public Multiset<String> getAlternativeNames(
+			final Multiset<NGram<String>> relevantNgrams,
+			final String currentName) {
+		// Get all alternative namings
+		final Multiset<String> nameAlternatives = ngramLM
+				.getAlternativeNamings(relevantNgrams, WILDCARD_TOKEN);
+		nameAlternatives.add(currentName); // Give the current identifier a
+											// chance...
+
+		// Prune naming alternatives
+		final Multiset<String> toKeep = TreeMultiset.create();
+
+		int seen = 0;
+		for (final Entry<String> ent : Multisets.copyHighestCountFirst(
+				nameAlternatives).entrySet()) {
+			if (seen > 1000) {
+				break;
+			}
+			toKeep.add(ent.getElement(), ent.getCount());
+			seen++;
+		}
+		toKeep.add(AbstractNGramLM.UNK_SYMBOL);
+		return toKeep;
+	}
+
 	public AbstractNGramLM getLM() {
 		return ngramLM;
+	}
+
+	/**
+	 * @param targetPositions
+	 * @param tokens
+	 * @return
+	 */
+	public Multiset<NGram<String>> getNgramsAtPosition(
+			final SortedSet<Integer> targetPositions, final List<String> tokens) {
+		final Multiset<NGram<String>> ngrams = HashMultiset.create();
+		for (final int ngramPos : targetPositions) {
+			for (int i = 0; i < ngramLM.getN(); i++) {
+				final int nGramPosition = ngramPos + i;
+				if (nGramPosition >= tokens.size()) {
+					break;
+				}
+				final NGram<String> ngram = NGram.constructNgramAt(
+						nGramPosition, tokens, ngramLM.getN());
+				ngrams.add(ngram);
+
+				if (ngram.size() <= 1) {
+					continue;
+				}
+			}
+		}
+
+		return ngrams;
 	}
 
 	/**
@@ -99,28 +156,30 @@ public abstract class AbstractIdentifierRenamings implements
 		final Multiset<NGram<String>> relevantNgrams = getSnippetNGrams(
 				scope.code, targetIdentifier);
 
-		// Get all alternative namings
-		final Multiset<String> nameAlternatives = ngramLM
-				.getAlternativeNamings(relevantNgrams, WILDCARD_TOKEN);
-		nameAlternatives.add(targetIdentifier); // Give the current identifier a
-												// chance...
-
-		// Prune naming alternatives
-		final Multiset<String> toKeep = TreeMultiset.create();
-
-		int seen = 0;
-		for (final Entry<String> ent : Multisets.copyHighestCountFirst(
-				nameAlternatives).entrySet()) {
-			if (seen > 1000) {
-				break;
-			}
-			toKeep.add(ent.getElement(), ent.getCount());
-			seen++;
-		}
-		toKeep.add(AbstractNGramLM.UNK_SYMBOL);
+		final Multiset<String> toKeep = getAlternativeNames(relevantNgrams,
+				targetIdentifier);
 
 		// calculate scores for each naming
 		return calculateScores(relevantNgrams, toKeep.elementSet(), scope);
+	}
+
+	/**
+	 * Return the renamings for a single token name binding
+	 * 
+	 * @param binding
+	 * @return
+	 */
+	public SortedSet<Renaming> getRenamings(final TokenNameBinding binding) {
+		// Get the snippet n-grams
+		final Multiset<NGram<String>> relevantNgrams = getSnippetNGrams(binding);
+
+		final String currentName = binding.getName();
+
+		final Multiset<String> toKeep = getAlternativeNames(relevantNgrams,
+				currentName);
+
+		// calculate scores for each naming
+		return calculateScores(relevantNgrams, toKeep.elementSet(), null);
 	}
 
 	/**
@@ -132,44 +191,43 @@ public abstract class AbstractIdentifierRenamings implements
 	 */
 	public Multiset<NGram<String>> getSnippetNGrams(final String snippet,
 			final String targetIdentifier) {
-		final Multiset<NGram<String>> ngrams = HashMultiset.create();
-
-		final List<FullToken> lst = checkNotNull(ngramLM).getTokenizer()
-				.getTokenListFromCode(snippet.toCharArray());
+		final List<String> lst = checkNotNull(ngramLM).getTokenizer()
+				.tokenListFromCode(snippet.toCharArray());
 
 		final SortedSet<Integer> identifierPositions = Sets.newTreeSet();
 		final List<String> sentence = Lists.newArrayList();
 
 		for (int i = 0; i < lst.size(); i++) {
-			final FullToken fullToken = lst.get(i);
-			sentence.add(fullToken.token);
-			if (fullToken.token.equals(targetIdentifier)
-					|| fullToken.token.contains("%" + targetIdentifier + "%")) {
+			final String token = lst.get(i);
+			sentence.add(token);
+			if (token.equals(targetIdentifier)
+					|| token.contains("%" + targetIdentifier + "%")) {
 				identifierPositions.add(i);
-				sentence.set(i, fullToken.token.replace(targetIdentifier,
-						WILDCARD_TOKEN));
+				sentence.set(i, token.replace(targetIdentifier, WILDCARD_TOKEN));
 			}
 		}
-
-		for (final int ngramPos : identifierPositions) {
-			for (int i = 0; i < ngramLM.getN(); i++) {
-				final int nGramPosition = ngramPos + i;
-				if (nGramPosition >= sentence.size()) {
-					break;
-				}
-				final NGram<String> ngram = NGram.constructNgramAt(
-						nGramPosition, sentence, ngramLM.getN());
-				ngrams.add(ngram);
-
-				if (ngram.size() <= 1) {
-					continue;
-				}
-			}
-		}
-
-		return ngrams;
+		return getNgramsAtPosition(identifierPositions, sentence);
 	}
 
+	/**
+	 * Return the n-gram for the current snippet.
+	 * 
+	 * @param binding
+	 * @return
+	 */
+	private Multiset<NGram<String>> getSnippetNGrams(
+			final TokenNameBinding binding) {
+		final TokenNameBinding renamed = binding.renameTo(WILDCARD_TOKEN);
+		return getNgramsAtPosition(Sets.newTreeSet(renamed.nameIndexes),
+				renamed.sourceCodeTokens);
+	}
+
+	/**
+	 * Return if the current token is an UNK under the n-gram LM.
+	 * 
+	 * @param token
+	 * @return
+	 */
 	public boolean isTrueUNK(final String token) {
 		return checkNotNull(ngramLM).getTrie().isUNK(token);
 	}
